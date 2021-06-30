c67fda3b59a004886387451c5ddb03ff88c5c8da
Nick Walter <smtp.nw@gmail.com>
2021-06-30 12:26:49 +1000

    Fix pkg update check

--- controls/packages_uptodate.rb
+++ controls/packages_uptodate.rb
@@ -2,7 +2,7 @@ control 'packages_uptodate' do
   title 'Core packages up to date'
   impact 1.0
 
-  describe command('yum update') do
-    its('stdout') { should match /No packages marked for update/ }
+  describe command('yum check-update') do
+    its('exit_status') { should eq 0 }
   end
 end
